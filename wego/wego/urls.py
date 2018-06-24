e0c207149e3e83658a4b3362e0d89cdea848b839
279665224@qq.com <asd19920909>
2018-06-24 20:43:58 +0800

    update file

--- wego/wego/urls.py
+++ wego/wego/urls.py
@@ -26,12 +26,6 @@ from wegoapi import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
-router.register(r'snippet_list',views.snippet_list)
-
-#
-
-
-
 
 
 # 使用自动的URL路由，让我们的API跑起来。
@@ -39,7 +33,5 @@ router.register(r'snippet_list',views.snippet_list)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^wego/$', views.snippet_list),
-    url(r'^wego/(?P<pk>[0-9]+)/$', views.snippet_detail),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- wego/wegoapi/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from wegoapi import views
-# test
-urlpatterns = [
-    url(r'^wego/$', views.snippet_list),
-    url(r'^wego/(?P<pk>[0-9]+)/$', views.snippet_detail),
-]
